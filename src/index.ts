fe0c1306d61dad672ed91f3c2a60194d6af98242
KhangNDD <khang_nguyen1@epam.com>
2023-04-23 17:58:10 +0700

    feat: Given a date and a token, return the portfolio value of that token in USD on that date

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,33 @@ let csvReadStream = fs.createReadStream(csvFilePath).pipe(csvParser());
 
 if (opts.token || opts.date) {
   if (opts.token && opts.date) {
+    let finalBalance;
+    let cacheDate, cacheTimestamp;
 
+    processCsvFile(csvReadStream, (row: Transaction) => {
+      if (opts.token === row.token) {
+        if (!cacheTimestamp || cacheTimestamp != row.timestamp) {
+          cacheTimestamp = row.timestamp;
+          
+          // Create a new Date object using the timestamp
+          const date = new Date(cacheTimestamp * 1000);
+  
+          // Get the date in a readable format
+          cacheDate = date.toLocaleDateString("en-US");
+        }
+
+        if (opts.date === cacheDate) {
+          if (finalBalance) {
+            finalBalance = calculateBalance(new Big(finalBalance), row.transaction_type, new Big(row.amount));
+          } else {
+            finalBalance = row.amount;
+          }
+        }
+      }
+    }, () => {
+      printPortfolio(opts.token, finalBalance);
+      console.timeEnd('Estimated time');
+    });
   } else if (opts.token) {
     let finalBalance;
 
@@ -45,6 +71,31 @@ if (opts.token || opts.date) {
     });
   } else if (opts.date) {
     console.log(`date: ${opts.date}`);
+    let portfolios = new Map<string, Big>();
+    let cacheDate, cacheTimestamp;
+
+    processCsvFile(csvReadStream, (row: Transaction) => {
+      if (!cacheTimestamp || cacheTimestamp != row.timestamp) {
+        cacheTimestamp = row.timestamp;
+        
+        // Create a new Date object using the timestamp
+        const date = new Date(cacheTimestamp * 1000);
+
+        // Get the date in a readable format
+        cacheDate = date.toLocaleDateString("en-US");
+      }
+
+      if (opts.date === cacheDate) {
+        if (portfolios.has(row.token)) {
+          portfolios.set(row.token, calculateBalance(portfolios.get(row.token), row.transaction_type, new Big(row.amount)));
+        } else {
+          portfolios.set(row.token, new Big(row.amount));
+        }
+      }
+    }, () => {
+      printPortfolios(portfolios);
+      console.timeEnd('Estimated time');
+    });
   }
 } else {
   let portfolios = new Map<string, Big>();
@@ -56,10 +107,7 @@ if (opts.token || opts.date) {
       portfolios.set(row.token, new Big(row.amount));
     }
   }, () => {
-    for (let [key, value] of portfolios) {
-      printPortfolio(key, value);
-    }
-
+    printPortfolios(portfolios);
     console.timeEnd('Estimated time');
   });
 }
@@ -78,6 +126,12 @@ function processCsvFile(csvReadStream, handler: Function, endHandler: Function) 
   .on('error', (error) => {
     console.error(`Something went wrong: ${error}`)
   });
+}
+
+function printPortfolios(portfolios: Map<string, Big>) {
+  for (let [key, value] of portfolios) {
+    printPortfolio(key, value);
+  }
 }
 
 function printPortfolio(token: string, balance: Big) {
